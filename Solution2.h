8d8b1ab5cf64241fcd633ba13e979610d430a5f8
Ahmed Farag Mourad <96184019+AhmedFaragMourad@users.noreply.github.com>
2022-11-18 14:43:00 +0200

    Create Solution2.h

---
+++ Solution2.h
@@ -0,0 +1,17 @@
+/**************************************************************************************************
+ Name        :Vehicle Control system.h
+ Author      : Ahmed Farag
+ Description :Student Information Management System
+ **************************************************************************************************/
+
+#ifndef DATABASE_H_
+#define DATABASE_H_
+void onvech(void);
+void ofvech(void);
+void trafficData();
+void ACtemp();
+void EngineTemp();
+void sysstat();
+char display (void);
+
+#endif /* DATABASE_H_ */
